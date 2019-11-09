fd08e92fbc421022dacea3a3b8e9b4d9ab806b56
krmechl <krmechl@gmail.com>
2019-11-09 11:48:22 -0500

    Add files via upload

---
+++ js/controllers.js
@@ -0,0 +1,106 @@
+angular.module('app.controllers', [])
+  
+.controller('accessionHealthCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('messageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('hoursCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('availabilityCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('profileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('activityCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('availability1Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reviewImageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reviewDocumentationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('pageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ js/routes.js
@@ -0,0 +1,133 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('menu.accessionHealth', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/accessionHealth.html',
+        controller: 'accessionHealthCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.message', {
+    url: '/page4',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/message.html',
+        controller: 'messageCtrl'
+      }
+    }
+  })
+
+  .state('menu.hours', {
+    url: '/page5',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/hours.html',
+        controller: 'hoursCtrl'
+      }
+    }
+  })
+
+  .state('menu.signup', {
+    url: '/page6',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/signup.html',
+        controller: 'signupCtrl'
+      }
+    }
+  })
+
+  .state('menu.availability', {
+    url: '/page7',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/availability.html',
+        controller: 'availabilityCtrl'
+      }
+    }
+  })
+
+  .state('menu.profile', {
+    url: '/page8',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl'
+      }
+    }
+  })
+
+  .state('menu.login', {
+    url: '/page9',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('menu.activity', {
+    url: '/page10',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/activity.html',
+        controller: 'activityCtrl'
+      }
+    }
+  })
+
+  .state('availability1', {
+    url: '/page11',
+    templateUrl: 'templates/availability1.html',
+    controller: 'availability1Ctrl'
+  })
+
+  .state('menu.reviewImage', {
+    url: '/page12',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reviewImage.html',
+        controller: 'reviewImageCtrl'
+      }
+    }
+  })
+
+  .state('menu.reviewDocumentation', {
+    url: '/page13',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reviewDocumentation.html',
+        controller: 'reviewDocumentationCtrl'
+      }
+    }
+  })
+
+  .state('page', {
+    url: '/page14',
+    templateUrl: 'templates/page.html',
+    controller: 'pageCtrl'
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/page1')
+
+
+});
